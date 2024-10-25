e14bab7b449d2e7fb5896a15a930a5b74158fa06
sulemanof <sul-dan@mail.ru>
2024-10-25 10:48:50 +0200

    Update asset view page

--- src/app/asset/[slug]/components/Header.tsx
+++ src/app/asset/[slug]/components/Header.tsx
@@ -3,14 +3,19 @@
 import { useRouter } from 'next/navigation';
 
 import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import Image from 'next/legacy/image';
 
 const Header = () => {
   const router = useRouter();
 
   return (
-    <div>
+    <header className="row-start-1 flex items-center justify-between gap-4 w-full">
+      <Link className="w-12" href="/">
+        <Image src="/nasa.png" alt="Logo" width={48} height={48} />
+      </Link>
       <Button onClick={() => router.back()}>Back</Button>
-    </div>
+    </header>
   );
 };
 
